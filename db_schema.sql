aa670cdb50c42a2ac45551e8b64a01c1bf89652a
ShahanS <mail.shahan@gmail.com>
2019-04-08 00:48:08 +0800

    Postman link added

--- db_schema.sql
+++ db_schema.sql
@@ -21,7 +21,3 @@ CREATE TABLE teacher_student (
   teacher_email VARCHAR(150),
   student_email VARCHAR(150)
 );
-
-INSERT INTO student (email, suspended) VALUES ('adam@student.com', 0),('alex@student.com', 0),('dash@student.com', 0),('david@student.com', 0),('don@student.com', 0),('feddy@student.com', 0),('kite@student.com', 0),('lorry@student.com', 0),('martin@student.com', 0),('quoine@student.com', 0),('roger@student.com', 0),('smith@student.com',0);
-INSERT INTO teacher  (email) VALUES ('barry@teacher.com'),('gill@teacher.com'),('Hans@teacher.com'),('helan@teacher.com'),('jeff@teacher.com'),('jerry@teacher.com'),('john@teacher.com'),('jonny@teacher.com'),('mathew@teacher.com'),('sarah@teacher.com'),('tom@teacher.com');
-INSERT INTO teachers_students VALUES ('barry@teacher.com','david@student.com'),('barry@teacher.com','kite@student.com'),('Hans@teacher.com','quoine@student.com'),('Hans@teacher.com','roger@student.com'),('jeff@teacher.com','roger@student.com'),('jeff@teacher.com','smith@student.com'),('jerry@teacher.com','alex@student.com'),('jerry@teacher.com','david@student.com'),('jerry@teacher.com','kite@student.com'),('jonny@teacher.com','quoine@student.com'),('jonny@teacher.com','roger@student.com');
